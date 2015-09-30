f808c5d4ca8148335ae9794075a3575a995e5d90
Rustam Mamedov <Rustam_Mamedov@EPRUSARW0837.saratov.epam.com>
2015-09-30 12:53:02 +0300

    fix book rating template, directive and style.

--- Library/Library/src/Scripts/directives/book-raiting.js
+++ Library/Library/src/Scripts/directives/book-raiting.js
@@ -5,6 +5,7 @@
         function bookRaiting(catalogService) {
             return {
                 restrict: 'A',
+                replace: true,
                 scope: {
                     bookId: '@',
                     bookRating: '@'
@@ -12,20 +13,39 @@
                 templateUrl: 'Partials/directives/book-rating.html',
                 link: function (scope, element, attr) {
 
-                        var uuiRaitingElement = $('.uui-rating');
+                        var uuiRatingElement = $('.uui-rating');
 
-                        uuiRaitingElement.uui_rating('init', { count: 5, animate_hover: true });
-                        uuiRaitingElement.uui_rating('set_value', scope.bookRating);
-                        $("#orderSwitch").bootstrapSwitch();
+                            uuiRatingElement.uui_rating('init', { count: 5, animate_hover: true });
+                            uuiRatingElement.uui_rating('set_value', scope.bookRating);
+                            $("#orderSwitch").bootstrapSwitch();
 
-                        element.on("click", function () {
-                            scope.actualRating = uuiRaitingElement.uui_rating('get_value');
+                        function changeRating () {
+
+                            scope.actualRating = uuiRatingElement.uui_rating('get_value');
                             catalogService.rateBook(scope.bookId, scope.actualRating);
-                            uuiRaitingElement.uui_rating('set_value', scope.bookRating);
-                        });
-                        
+                            uuiRatingElement.uui_rating('set_value', scope.actualRating);
+                        }
+
+                        function selectRating () {
+
+                            uuiRatingElement.uui_rating('set_value', 0);
+                            uuiRatingElement.toggleClass('blue yellow');
+                        }
+
+                        function cancelSelecting () {
+
+                            uuiRatingElement.toggleClass('blue yellow');
+                            uuiRatingElement.uui_rating('set_value', scope.bookRating);
+                        }
+
+                        element.on('click', changeRating);
+
+                        element.on('mouseenter', selectRating);
+
+                        element.on('mouseleave', cancelSelecting);
+
                         scope.$watch('bookId', function () {
-                            uuiRaitingElement.uui_rating('set_value', scope.bookRating);
+                            uuiRatingElement.uui_rating('set_value', scope.bookRating);
                         });
                 }
             };
