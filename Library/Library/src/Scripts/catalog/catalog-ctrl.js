baef4d111b9940e9a7d484a5cf68bdd6e0c3852b
Aleksey Prokofiev <Aleksei_Prokofev@epam.com>
2015-10-02 18:30:33 +0300

    Evening

--- Library/Library/src/Scripts/catalog/catalog-ctrl.js
+++ Library/Library/src/Scripts/catalog/catalog-ctrl.js
@@ -6,12 +6,16 @@
 
     function catalogCtrl($state, $stateParams, catalogService, threadsService) {
         var vm = this;
+        vm.panelSize = 300;
 
         if ($stateParams.search) {
             catalogService.getBySearchString($stateParams.search).then(function (response) {
                 vm.books = response.data;
                 if (vm.books.length) {
                     vm.setActive(vm.books[0].id);
+
+                    vm.grad = 0;
+                    vm.tz = Math.round((vm.panelSize / 2) / Math.tan(Math.PI / vm.books.length));
                 } else {
                     $state.go('404');
                 }
@@ -19,17 +23,43 @@
         } else {
             catalogService.getAllBooks().then(function (response) {
                 vm.books = response.data;
-                vm.setActive(vm.books[0].id);
+                if (vm.books.length) {
+                    vm.setActive(vm.books[0].id);
+
+                    vm.grad = 0;
+                    vm.tz = Math.round((vm.panelSize / 2) / Math.tan(Math.PI / vm.books.length));
+                } else {
+                    $state.go('404');
+                }
             });
         }
 
+
+
+
         $('#book-carousel').on('wheel', function (e) {
             if (e.originalEvent.deltaY > 0) {
-                console.log('up!');
+                vm.grad += (360 / vm.books.length);
             } else {
-                console.log('down!');
+                vm.grad -= (360 / vm.books.length);
             }
+
+            $(this).css({ transform: 'rotateX(' + vm.grad + 'deg)' });
         });
+
+        vm.style = function (index, count) {
+            var grad = (360 / count) * index;
+
+            return { transform: 'rotateX( ' + grad + 'deg ) translateZ( ' + vm.tz + 'px )' };
+        }
+
+        vm.rotate = function () {
+            return { transform: 'rotateX( ' + vm.grad + 'deg ) translateZ( -' + vm.tz + 'px )' };
+        }
+
+
+
+
 
         vm.setActive = function (bookId) {
             vm.activeBook = _.find(vm.books, { id: bookId });
@@ -41,12 +71,6 @@
             }
         }
 
-        vm.zIndex = function (bookId) {
-            if (vm.isActive(bookId)) {
-                if (!vm.mouseIn) {
-                    return { 'z-index': '1000' };
-                }
-            }
-        }
+
     }
 }());
